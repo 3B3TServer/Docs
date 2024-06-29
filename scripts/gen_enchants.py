d58c0f2e3a0d27dca96d2429ee0485b1066d1e72
masmc05 <masmc05@gmail.com>
2024-06-30 00:42:21 +0300

    Enchants

---
+++ scripts/gen_enchants.py
@@ -0,0 +1,44 @@
+# Read data/enchants.json and generate the enchantments
+# To a mkdocs compatible format
+
+import json
+import os
+
+# Example enchantment
+# {
+#     "appliesTo": "Элитры",
+#     "highestLevel": 5,
+#     "description": "Шанс отравить противника и обретение регенерации.",
+#     "display": "Приговор"
+# }
+groupToNames = {
+    "SIMPLE": "Обычный",
+    "UNIQUE": "Редкий",
+    "ELITE": "Элитный",
+    "ULTIMATE": "Высший",
+    "LEGENDARY": "Легендарный",
+    "FABLED": "Мифический",
+}
+
+
+def main():
+    print(1)
+    with open("data/enchants.json", encoding="utf-8") as f:
+        data = json.load(f)
+    md_string = ""
+    # the keys are group ids, values are enchantments
+    for group in groupToNames.keys():
+        group_name = groupToNames[group]
+        md_string += f"\n=== \"{group_name}\"\n"
+        enchantment_string = "\n"
+        for enchantment in data[group]:
+            enchantment_string += f"    !!! enchants-{group.lower()} \"{enchantment['display']}\"\n"
+            enchantment_string += f"        {enchantment['description']}<br><br>"
+            enchantment_string += f"**Применяется к:** {enchantment['appliesTo']}<br>"
+            enchantment_string += f"**Максимальный уровень:** {enchantment['highestLevel']}\n\n"
+        md_string += enchantment_string
+
+    with open("../docs/enchantments.md", "w", encoding="utf-8") as f:
+        f.write(md_string)
+
+main()
