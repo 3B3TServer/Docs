0a62858e1cec3d699e58bd6082e3958e05f3d4fc
masmc05 <masmc05@gmail.com>
2024-06-30 00:46:22 +0300

    Remove unnecesary whitespace

--- scripts/gen_enchants.py
+++ scripts/gen_enchants.py
@@ -22,23 +22,23 @@ groupToNames = {
 
 
 def main():
-    print(1)
     with open("data/enchants.json", encoding="utf-8") as f:
         data = json.load(f)
     md_string = ""
     # the keys are group ids, values are enchantments
     for group in groupToNames.keys():
         group_name = groupToNames[group]
-        md_string += f"\n=== \"{group_name}\"\n"
+        md_string += f"=== \"{group_name}\""
         enchantment_string = "\n"
         for enchantment in data[group]:
             enchantment_string += f"    !!! enchants-{group.lower()} \"{enchantment['display']}\"\n"
             enchantment_string += f"        {enchantment['description']}<br><br>"
             enchantment_string += f"**Применяется к:** {enchantment['appliesTo']}<br>"
-            enchantment_string += f"**Максимальный уровень:** {enchantment['highestLevel']}\n\n"
+            enchantment_string += f"**Максимальный уровень:** {enchantment['highestLevel']}\n"
         md_string += enchantment_string
 
     with open("../docs/enchantments.md", "w", encoding="utf-8") as f:
         f.write(md_string)
+
 
 main()
